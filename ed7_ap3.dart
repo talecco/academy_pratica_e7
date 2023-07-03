50292317c6be11455ecf9c1baf87610343a6b757
Thales Borges <63830404+talecco@users.noreply.github.com>
2023-07-03 01:46:33 -0300

    Create ed7_ap3.dart

---
+++ ed7_ap3.dart
@@ -0,0 +1,29 @@
+class Produto {
+  String nome;
+  double preco;
+
+  Produto(this.nome, this.preco);
+
+  double desconto(double percentual) {
+    double valorDesconto = (percentual / 100) * preco;
+    double novoPreco = preco - valorDesconto;
+    return novoPreco;
+  }
+}
+
+void main() {
+  List<Produto> produtos = [
+    Produto("Produto A", 100.0),
+    Produto("Produto B", 200.0),
+    Produto("Produto C", 300.0),
+    Produto("Produto D", 400.0),
+    Produto("Produto E", 500.0),
+  ];
+
+  double percentualDesconto = 10.0; // Exemplo de desconto de 10%
+
+  for (Produto produto in produtos) {
+    double novoPreco = produto.desconto(percentualDesconto);
+    print("Novo preço do produto ${produto.nome} (com desconto): $novoPreco");
+  }
+}
